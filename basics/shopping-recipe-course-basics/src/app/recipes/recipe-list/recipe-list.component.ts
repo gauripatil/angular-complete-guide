b8fc945a255fd36faaa22462e835d2a9351d288c
Gauri Patil <gauri.be0811@gmail.com>
2024-02-29 21:07:56 +0530

    Added recipe model & binding

--- basics/shopping-recipe-course-basics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basics/shopping-recipe-course-basics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,5 +12,10 @@ import { RecipeItemComponent } from './recipe-item/recipe-item.component';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "A Test Recipe Description", "https://static.toiimg.com/photo/77151731.cms"),
+    new Recipe("A Test Recipe", "A Test Recipe Description", "https://static.toiimg.com/photo/77151731.cms"),
+    new Recipe("A Test Recipe", "A Test Recipe Description", "https://static.toiimg.com/photo/77151731.cms")
+  ];
 
 }
