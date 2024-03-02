ac75c6def7640da78b1eb594fd75c2b33f87a6b3
Gauri Patil <gauri.be0811@gmail.com>
2024-03-02 21:28:58 +0530

    Added Ingredients & Shopping List edit

--- basics/shopping-recipe-course-basics/src/app/shopping-list/shopping-list.component.ts
+++ basics/shopping-recipe-course-basics/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RecipesComponent } from '../recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -12,5 +13,11 @@ import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+
+  ingredients: Ingredient[] = [
+    new Ingredient("Ingredient 1", "1000"),
+    new Ingredient("Ingredient 2", "2000"),
+    new Ingredient("Ingredient 3", "3000")
+  ];
 
 }
