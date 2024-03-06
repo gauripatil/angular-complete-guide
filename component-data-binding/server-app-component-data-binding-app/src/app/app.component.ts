d5aaab5b83ac305a097956dd25213dc4f425750f
Gauri Patil <gauri.be0811@gmail.com>
2024-03-06 22:43:33 +0530

    Component Data Binding App started

---
+++ component-data-binding/server-app-component-data-binding-app/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { ServerComponent } from './server/server.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule,
+    ServerComponent,
+    CockpitComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'server-app-component-data-binding-app';
+  serverElements: any = [];
+  newServerName: string = '';
+  newServerContent: string = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
