6fd536a5b7248b1ac97e139e96e46e9f90589352
Gauri Patil <gauri.be0811@gmail.com>
2024-03-14 00:06:55 +0530

    Sec5 - Component & data binding - Getting access to the Template & DOM with @ViewChild

--- Components-databinding-deep-dive/server-app-component-data-binding-app/src/app/cockpit/cockpit.component.ts
+++ Components-databinding-deep-dive/server-app-component-data-binding-app/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -12,21 +12,22 @@ import { FormsModule } from '@angular/forms';
 })
 export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @ViewChild('serverContentInput') serverContentInput: ElementRef | undefined;
   // newServerName: string = '';
-  newServerContent: string = '';
+  // newServerContent: string = '';
 
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput?.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) { 
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput?.nativeElement.value
     });
   }
 
