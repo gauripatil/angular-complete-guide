5d25ba2e1c2867c198964ccc0225e0f51b2c5c42
Gauri Patil <gauri.be0811@gmail.com>
2024-03-22 17:33:36 +0530

    Sec6 - Component & Data binding - Shopping list app practice - passing data with Event and property binding

--- Basics-sample-code/shopping-recipe-course-basics/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Basics-sample-code/shopping-recipe-course-basics/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -12,5 +12,10 @@ export class RecipeItemComponent {
 
   @Input('recipe')
   recipe!: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
+
+  onSelected(): void {
+    this.recipeSelected.emit();
+   }
 
 }
