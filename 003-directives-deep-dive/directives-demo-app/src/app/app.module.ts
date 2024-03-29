3d6cd812c4511e9165ad21bfa5d49ece140506e1
Gauri Patil <gauri.be0811@gmail.com>
2024-03-29 20:19:16 +0530

    Sec7 - Directives - Building structural directive

--- 003-directives-deep-dive/directives-demo-app/src/app/app.module.ts
+++ 003-directives-deep-dive/directives-demo-app/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { AppBetterHighlightDirective } from './better-highlight/app-better-highlight.directive';
+import { UnlessDirective } from './structural-directive/unless.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
-    AppBetterHighlightDirective
+    AppBetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
